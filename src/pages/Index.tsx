9e7f9be44fbe3dbc97018a5c39dbbaab18373cfd
BerrySeriousCoder <harsh100xdev.work@gmail.com>
2025-05-03 14:15:00 +0530

    Add comprehensive UI components and initial pages

---
+++ src/pages/Index.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import BentoGrid from '../components/BentoGrid';
+import FAQ from '../components/FAQ';
+import CTA from '../components/CTA';
+import Footer from '../components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <BentoGrid />
+      <FAQ />
+      <CTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
